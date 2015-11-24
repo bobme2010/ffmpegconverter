51fc3700b05a31654f686c0319f57c5cf0cfdf7d
wildon <bobme@sina.com>
2015-11-24 20:44:51 +0800

    Change-Id: I7f12fba21b94d37f1150463d82dc0cddca05f9a0

---
+++ ffmpeg_converter/FFMPEGConverter/src/Test.java
@@ -0,0 +1,46 @@
+import it.sauronsoftware.jave.AudioAttributes;
+import it.sauronsoftware.jave.Encoder;
+import it.sauronsoftware.jave.EncoderException;
+import it.sauronsoftware.jave.EncodingAttributes;
+import it.sauronsoftware.jave.InputFormatException;
+
+import java.io.File;
+
+//参考文档网址
+//http://tj007-bo.iteye.com/blog/435740
+
+
+//http://www.sauronsoftware.it/projects/jave/index.php
+
+
+public class Test {
+
+	public static void main(String[] args) throws Exception 
+	{
+		File source = new File("source.wav");
+		File target = new File("target.mp3");
+		
+		AudioAttributes audio = new AudioAttributes();
+		audio.setCodec("libmp3lame");
+		
+		//audio.setBitRate(new Integer(128000));
+		audio.setBitRate(new Integer(128000));
+		
+		//audio.setChannels(new Integer(2));
+		audio.setChannels(new Integer(1));
+		
+		//audio.setSamplingRate(new Integer(44100));
+		audio.setSamplingRate(new Integer(44100));
+		
+		EncodingAttributes attrs = new EncodingAttributes();
+		attrs.setFormat("mp3");
+		
+		attrs.setAudioAttributes(audio);
+		Encoder encoder = new Encoder();
+		
+		encoder.encode(source, target, attrs);
+	}
+	
+	
+	
+}
